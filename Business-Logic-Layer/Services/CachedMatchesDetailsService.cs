31d68a178db703bff4fb41157216185790e80b9e
Bartosz Truszkowski <truszkoo@gmail.com>
2024-07-22 01:43:57 +0200

    Added Matches caching service, correction commit V2

---
+++ Business-Logic-Layer/Services/CachedMatchesDetailsService.cs
@@ -0,0 +1,37 @@
+﻿using Business_Logic_Layer.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Services
+{
+    public class CachedMatchesDetailsService : IMatchDetailsService
+    {x
+
+        private readonly MatchDetailsService _matchDetailsService;
+        private readonly IMemoryCache _memoryCache;
+        public CachedMatchesDetailsService(MatchDetailsService matchDetailsService, IMemoryCache memoryCache)
+        {
+            _matchDetailsService = matchDetailsService;
+            _memoryCache = memoryCache;
+        }
+
+        public Task<MatchDto> GetMatchDetailsByMatchIdAsync(string matchId)
+        {
+            // Caching logic to be implemented
+            
+            return  _matchDetailsService.GetMatchDetailsByMatchIdAsync(matchId);
+
+        }
+
+        public Task<List<MatchDto>> GetMatchDetailsListByMatchIdsAsync(IEnumerable<string> matchIdsList, string SummonerPUUID)
+        {
+            // Caching logic to be implemented
+
+            return _matchDetailsService.GetMatchDetailsListByMatchIdsAsync(matchIdsList, SummonerPUUID);
+        }
+    }
+}
---
+++ webapi/Controllers/ServicesLifeCycleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class ServicesLifeCycleController : ControllerBase
+    {
+
+        public ServicesLifeCycleController()
+        {
+                
+        }
+    }
+}
